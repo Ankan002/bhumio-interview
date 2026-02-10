87c2bb995453e1a9aeaafc900fb5cbc75bda65b9
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2026-02-10 21:09:35 +0530

    chore: :wrench: fix theme provider

--- components/providers/theme-provider/theme-provider.tsx
+++ components/providers/theme-provider/theme-provider.tsx
@@ -6,7 +6,18 @@ const ThemeProvider = ({
     children,
     ...props
 }: React.ComponentProps<typeof NextThemesProvider>) => {
-    return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+    return (
+        <NextThemesProvider
+            {...props}
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="theme"
+        >
+            {children}
+        </NextThemesProvider>
+    );
 };
 
 export default ThemeProvider;
