92703699b01ee4ac5ef09c3c3a2274240dd864e4
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2026-02-11 22:28:55 +0530

    feat: :sparkles: add false validation hook

---
+++ services/api/false-validation/index.ts
@@ -0,0 +1,1 @@
+export * from "./validation-that-lies";
---
+++ services/api/false-validation/validation-that-lies.ts
@@ -0,0 +1,32 @@
+import { JsonApiResponse } from "@/types/api";
+import { useMutation } from "@tanstack/react-query";
+
+interface ValidationThatLiesArgs {
+    email: string;
+    amount: number;
+}
+
+const validationThatLies = async (args: ValidationThatLiesArgs) => {
+    const response = await fetch("/api/validation-that-lies", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(args),
+    });
+
+    const data = (await response.json()) as JsonApiResponse;
+
+    if (!data.success) {
+        throw new Error(data.error);
+    }
+
+    return true;
+};
+
+export const useValidationThatLies = () => {
+    return useMutation({
+        mutationFn: validationThatLies,
+        mutationKey: ["validation-that-lies"],
+    });
+};
